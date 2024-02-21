94e2cc31a5feddd926a068d577a91559c322f2e7
ram-elife <ramrachai@elifetransfer.com>
2024-02-21 18:27:16 +0600

    categoryText added

--- dev/2/script.js
+++ dev/2/script.js
@@ -1,30 +1,47 @@
 // JavaScript code to generate HTML and CSS dynamically
 
-const api_url = "https://raw.githubusercontent.com/Ramrachai/echologyx-test/master/dev/echologyx/Ram/products.json"
+const api_url = "https://raw.githubusercontent.com/Ramrachai/echologyx-test/master/dev/echologyx/Ram/products.json";
 
-const icon1 = "https://res.cloudinary.com/ramrachai/image/upload/v1708434932/ou9hjlu8orzfig3o5jd2.svg"
-const icon2 = "https://res.cloudinary.com/ramrachai/image/upload/v1708434932/x8mzqfnnxa55opyfvuft.svg"
-const icon3 = "https://res.cloudinary.com/ramrachai/image/upload/v1708434932/gstunorpjfyaugsf97sa.svg"
-
-
-let productData = []
+let productData = [];
 
 async function getProducts() {
-  let res = await fetch(api_url)
-  let data = await res.json()
-  console.log("data-- ", data)
-  productData = await data
+  let res = await fetch(api_url);
+  let data = await res.json();
+  console.log("data-- ", data);
+  productData = await data;
 
   renderCategoriesAndProducts();
 }
 
 function renderCategoriesAndProducts() {
   // Clear the current body
-  document.body.innerHTML = '';
+  document.body.innerHTML = "";
 
   // Generate CSS styles
-  const style = document.createElement('style');
+  const style = document.createElement("style");
   style.textContent = `
+
+    .header {
+      color: #fff;
+      padding: 20px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 40px;
+      width: 80%;
+      margin: 0 auto; 
+      background: #f5f5f5; 
+      height: 80px;
+    }
+    
+    .header img {
+      width: 40px;
+    }
+    
+    .header p {
+      color: #464646;
+    }
+
     .category-card, .product-card {
       border: 1px solid #ccc;
       padding: 10px;
@@ -33,6 +50,9 @@ function renderCategoriesAndProducts() {
       display: inline-block;
       cursor: pointer;
       position: relative;
+    }
+    .category-card{
+      background: #1e9fc4;
     }
     .hidden {
       display: none;
@@ -45,59 +65,113 @@ function renderCategoriesAndProducts() {
       height: 20px;
       fill: green;
     }
+    .category-icon {
+      width: 30px;
+      height: 30px;
+      margin-right: 5px;
+    }
   `;
   document.head.appendChild(style);
 
+
+  // header section  
+
+  // Create a CSS class for styling the header
+  const headerClass = "header";
+
+  // header section
+  const header = document.createElement("header");
+  header.classList.add(headerClass); // Add the class to the header
+
+  const logo = document.createElement("img");
+  logo.src =
+    "https://aktionen.consorsbank.de/ev/ev_beste_angebote/assets/images/logo_white_consors.png";
+  logo.alt = "Logo";
+  header.appendChild(logo);
+
+  const headerText = document.createElement("p");
+  const boldText = document.createElement("b");
+  boldText.textContent = "Unsere besten Angebote";
+  headerText.appendChild(boldText);
+  headerText.innerHTML += "<br>";
+
+  const spanText = document.createElement("span");
+  spanText.textContent = " Willkommen bei der Consorsbank ";
+  headerText.appendChild(spanText);
+
+  header.appendChild(headerText);
+
+  const headerRight = document.createElement("img");
+  headerRight.src =
+    "https://aktionen.consorsbank.de/ev/ev_beste_angebote/assets/images/bester_online_broker_2023.jpg";
+  headerRight.alt = "Header Right";
+  headerRight.style.marginLeft = "auto";
+  header.appendChild(headerRight);
+
+  // Append the header to the document body
+  document.body.appendChild(header);
+
+
+
+
   // Create categories container
-  const categoriesContainer = document.createElement('div');
-  categoriesContainer.id = 'categories';
+  const categoriesContainer = document.createElement("div");
+  categoriesContainer.id = "categories";
   document.body.appendChild(categoriesContainer);
 
-  // Create products container
-  const productsContainer = document.createElement('div');
-  productsContainer.id = 'products';
-  document.body.appendChild(productsContainer);
-
   // Create result container
-  const resultContainer = document.createElement('div');
-  resultContainer.id = 'result';
+  const resultContainer = document.createElement("div");
+  resultContainer.id = "result";
   document.body.appendChild(resultContainer);
 
+  // Create products container
+  const productsContainer = document.createElement("div");
+  productsContainer.id = "products";
+  document.body.appendChild(productsContainer);
+
+
+
   // Render categories
-  const categories = [...new Set(productData.map(product => product.category))];
-  categories.forEach(category => {
-    const categoryCard = document.createElement('div');
-    categoryCard.classList.add('category-card');
-    categoryCard.textContent = category;
+  const categories = [...new Set(productData.map((product) => product.category))];
+  categories.forEach((category, index) => {
+    const product = productData.find((product) => product.category === category);
+    const categoryCard = document.createElement("div");
+    categoryCard.classList.add("category-card");
+
+    const categoryTitle = document.createElement("h2") 
+    categoryTitle.textContent = category;
+    categoryCard.appendChild(categoryTitle)
+
+
     let isHidden = false;
-    const checkIcon = createCheckmarkSVG(); // Create checkmark SVG
-    categoryCard.appendChild(checkIcon); // Append checkmark to category card
-    categoryCard.addEventListener('click', () => {
+    const categoryIcon = createCategoryIcon(product); // Create category icon
+    if (categoryIcon) {
+      categoryCard.appendChild(categoryIcon); // Append category icon to category card
+    }
+    const checkmark = createCheckmarkIcon(); // Create checkmark icon
+    categoryCard.appendChild(checkmark); // Append checkmark icon to category card
+    checkmark.style.display = "block"; // Display the checkmark by default
+    categoryCard.addEventListener("click", () => {
       isHidden = !isHidden;
       toggleProducts(category, isHidden);
       // Toggle checkmark visibility based on isHidden value
-      checkIcon.style.display = isHidden ? 'none' : 'block';
+      checkmark.style.display = isHidden ? "none" : "block";
     });
     categoriesContainer.appendChild(categoryCard);
-
-    // Initially hide checkmark if products are hidden
-    if (isHidden) {
-      checkIcon.style.display = 'none';
-    }
   });
 
   // Render products
   function renderProducts(products) {
-    productsContainer.innerHTML = '';
-    products.forEach(product => {
-      const productCard = document.createElement('div');
-      productCard.classList.add('product-card');
+    productsContainer.innerHTML = "";
+    products.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card");
       productCard.id = `product-${product.id}`; // Set unique ID for each product card
       productCard.innerHTML = `
         <h3>${product.title}</h3>
         <p>${product.subtitle}</p>
         <ul>
-          ${product.features.map(feature => `<li>${feature}</li>`).join('')}
+          ${product.features.map((feature) => `<li>${feature}</li>`).join("")}
         </ul>
         <button>${product.buttonText}</button>
         <img width="100" src="${product.imgLink}" alt="${product.title}">
@@ -105,49 +179,69 @@ function renderCategoriesAndProducts() {
       productsContainer.appendChild(productCard);
     });
 
-    updateResult(products.filter(product => !productCardIsHidden(product.id)).length);
+    updateResult(
+      products.filter((product) => !productCardIsHidden(product.id)).length
+    );
   }
 
   function toggleProducts(category, isHidden) {
-    const filteredProducts = productData.filter(product => product.category === category);
-    filteredProducts.forEach(product => {
+    const filteredProducts = productData.filter(
+      (product) => product.category === category
+    );
+    filteredProducts.forEach((product) => {
       const productCard = document.getElementById(`product-${product.id}`);
       if (productCard) {
         if (isHidden) {
-          productCard.classList.add('hidden');
+          productCard.classList.add("hidden");
         } else {
-          productCard.classList.remove('hidden');
+          productCard.classList.remove("hidden");
         }
       }
     });
 
-    updateResult(productData.filter(product => !productCardIsHidden(product.id)).length);
+    updateResult(
+      productData.filter((product) => !productCardIsHidden(product.id)).length
+    );
   }
 
   // Update result container with the number of visible products
   function updateResult(count) {
-    resultContainer.textContent = `Result: ${count} Product${count !== 1 ? 's' : ''}`;
+    resultContainer.textContent = `Result: ${count} Product${count !== 1 ? "s" : ""
+      }`;
   }
 
   // Check if a product card is hidden
   function productCardIsHidden(id) {
     const productCard = document.getElementById(`product-${id}`);
-    return productCard && productCard.classList.contains('hidden');
+    return productCard && productCard.classList.contains("hidden");
   }
 
-  // Create checkmark SVG
-  function createCheckmarkSVG() {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.classList.add('checkmark');
-    svg.setAttribute('width', '20');
-    svg.setAttribute('height', '20');
-    svg.setAttribute('viewBox', '0 0 35 35');
-    svg.setAttribute('fill', 'none');
+  // Create category icon
+  function createCategoryIcon(product) {
+    if (product && product.categoryIcon) {
+      const icon = document.createElement("img");
+      icon.classList.add("category-icon");
+      icon.src = product.categoryIcon;
+      return icon;
+    }
+    return null;
+  }
+
+  // Create checkmark icon
+  function createCheckmarkIcon() {
+    const checkmark = document.createElement("i");
+    checkmark.classList.add("checkmark");
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "20");
+    svg.setAttribute("height", "20");
+    svg.setAttribute("viewBox", "0 0 35 35");
+    svg.setAttribute("fill", "none");
     svg.innerHTML = `
       <circle cx="17.5" cy="17.5" r="16.5" fill="#61AF34" stroke="white" stroke-width="2"/>
       <path d="M15.6083 27L8 19.7309L9.99623 18.283L15.2316 23.2768L25.7778 8L28 9.06376L15.6083 27Z" fill="white"/>
     `;
-    return svg;
+    checkmark.appendChild(svg);
+    return checkmark;
   }
 
   // Initially show all products
