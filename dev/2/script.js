45516a84b54ca3fc27e85ca609fed7d53bbb8d37
ram-elife <ramrachai@elifetransfer.com>
2024-02-21 19:36:09 +0600

    95% working. only category title in product list is left

--- dev/2/script.js
+++ dev/2/script.js
@@ -21,6 +21,16 @@ function renderCategoriesAndProducts() {
   const style = document.createElement("style");
   style.textContent = `
 
+   * {
+      margin: 0; 
+      padding: 0; 
+   }
+    body {
+
+      box-sizing: border-box; 
+      font-family: Arial;
+    }
+
     .header {
       color: #fff;
       padding: 20px;
@@ -29,7 +39,7 @@ function renderCategoriesAndProducts() {
       align-items: center;
       gap: 40px;
       width: 80%;
-      margin: 0 auto; 
+      margin: 2rem auto; 
       background: #f5f5f5; 
       height: 80px;
     }
@@ -42,18 +52,91 @@ function renderCategoriesAndProducts() {
       color: #464646;
     }
 
-    .category-card, .product-card {
-      border: 1px solid #ccc;
-      padding: 10px;
-      margin: 10px;
-      width: 200px;
-      display: inline-block;
-      cursor: pointer;
-      position: relative;
+    #categories {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 1rem;
+      width: 80%; 
+      margin: 0 auto; 
+      flex-wrap: wrap; 
     }
     .category-card{
       background: #1e9fc4;
+      flex: 1; 
+      min-height: 200px;
+      display:flex; 
+      justify-content: center;
+      align-items:center; 
+      flex-direction: column;
+      color: white;
+      gap: 12px;
     }
+    .category-title {
+      width: 80%; 
+      text-align: left; 
+      margin: 16px auto;
+      padding-top: 1rem;
+    }
+    .category-card p {
+      font-size: 13px;
+      text-align: center;
+    }
+
+    .category-card, .product-card {
+      margin: 16px;
+      width: 100%;
+      cursor: pointer;
+      position: relative;
+    }
+
+    div#result {
+      width: 80%;
+      margin: 1rem auto;
+      font-size: 18px;
+    }
+
+    div#products {
+      width: 80%;
+      margin: 1rem auto;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+
+    .product-card {
+      background: #f5f5f5;
+      display: flex;
+      gap: 4rem;
+      align-items: center;
+  }
+
+    .product-card div {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    .product-card div li {
+        list-style: none;
+        font-size: 16px;
+        margin-bottom: 12px;
+    }
+
+    .product-card div button{
+      background: #3db1c9;
+      border: none;
+      padding: 1rem 2rem;
+      font-size: 1.2rem;
+      color: white;
+      width: fit-content;
+      border-radius: 6px;
+    }
+    .product-card div h3 {
+      font-size: 2rem;
+    }
+
+
     .hidden {
       display: none;
     }
@@ -66,9 +149,8 @@ function renderCategoriesAndProducts() {
       fill: green;
     }
     .category-icon {
-      width: 30px;
-      height: 30px;
-      margin-right: 5px;
+      width: 50px;
+      height: 50px;
     }
   `;
   document.head.appendChild(style);
@@ -112,7 +194,10 @@ function renderCategoriesAndProducts() {
   document.body.appendChild(header);
 
 
-
+  const categoryTitle = document.createElement("h2")
+  categoryTitle.textContent = "Wofür interessieren Sie sich?"
+  categoryTitle.classList.add("category-title")
+  document.body.appendChild(categoryTitle)
 
   // Create categories container
   const categoriesContainer = document.createElement("div");
@@ -138,16 +223,22 @@ function renderCategoriesAndProducts() {
     const categoryCard = document.createElement("div");
     categoryCard.classList.add("category-card");
 
-    const categoryTitle = document.createElement("h2") 
-    categoryTitle.textContent = category;
-    categoryCard.appendChild(categoryTitle)
-
-
-    let isHidden = false;
     const categoryIcon = createCategoryIcon(product); // Create category icon
     if (categoryIcon) {
       categoryCard.appendChild(categoryIcon); // Append category icon to category card
     }
+
+    const categoryTitle = document.createElement("h2")
+    categoryTitle.textContent = category;
+    categoryCard.appendChild(categoryTitle)
+
+    const categoryText = document.createElement("p")
+    categoryText.textContent = product.categoryText
+    categoryCard.appendChild(categoryText)
+    console.log("category ==", category, "product==", product)
+
+    let isHidden = false;
+
     const checkmark = createCheckmarkIcon(); // Create checkmark icon
     categoryCard.appendChild(checkmark); // Append checkmark icon to category card
     checkmark.style.display = "block"; // Display the checkmark by default
@@ -168,13 +259,16 @@ function renderCategoriesAndProducts() {
       productCard.classList.add("product-card");
       productCard.id = `product-${product.id}`; // Set unique ID for each product card
       productCard.innerHTML = `
-        <h3>${product.title}</h3>
-        <p>${product.subtitle}</p>
-        <ul>
-          ${product.features.map((feature) => `<li>${feature}</li>`).join("")}
-        </ul>
-        <button>${product.buttonText}</button>
-        <img width="100" src="${product.imgLink}" alt="${product.title}">
+      
+        <img width="40%" src="${product.imgLink}" alt="${product.title}">
+        <div> 
+          <h3>${product.title}</h3>
+          <p>${product.subtitle}</p>
+          <ul>
+            ${product.features.map((feature) => `<li> <span style="color: green;">&#x2713;</span> ${feature}</li>`).join("")}
+          </ul>
+          <button>${product.buttonText} &rang; </button>
+        </div>
       `;
       productsContainer.appendChild(productCard);
     });
@@ -206,8 +300,7 @@ function renderCategoriesAndProducts() {
 
   // Update result container with the number of visible products
   function updateResult(count) {
-    resultContainer.textContent = `Result: ${count} Product${count !== 1 ? "s" : ""
-      }`;
+    resultContainer.innerHTML = `Wir haben <b> ${count} Produkte </b> gefunden, die zu Ihren Interessen passen.`;
   }
 
   // Check if a product card is hidden
